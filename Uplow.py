e0685cec1223dcdb7d8b45bba9057c4c1f060c43
NaikRedya <89027482+NaikRedya@users.noreply.github.com>
2021-09-19 19:03:09 +0530

    Create Uplow.py

---
+++ Uplow.py
@@ -0,0 +1,10 @@
+def ul(s):
+   d={"UPPER_CASE":0,"LOWER_CASE":0}
+   for i in s:
+      if c.isupper(0:
+          d["UPPER_CASE"]+=1
+      elif c.islower(0:
+          d["LOWER_CASE"]+=1
+   print("No of Upper class characters:",d["UPPER_CASE])
+   print("No of lower class characters:",d["LOWER_CASE])
+ul('The quick Brown Fox')
